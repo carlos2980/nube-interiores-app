7839a63d4eea31b32655387fc138ff1dd5c6b71d
carlos2980 <38896587+carlos2980@users.noreply.github.com>
2026-05-30 20:43:09 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -322,7 +322,15 @@ return (
           </button>
 
           <h1 className="font-semibold text-lg">Nube Interiores</h1>
-          <ShoppingCart size={22} />
+          <div className="relative">
+  <ShoppingCart size={22} />
+
+  {carrito.length > 0 && (
+    <span className="absolute -top-2 -right-2 bg-red-600 text-white text-[10px] font-bold w-5 h-5 rounded-full flex items-center justify-center">
+      {carrito.length}
+    </span>
+  )}
+</div>
         </div>
 
         <main className="px-5 pb-28">
